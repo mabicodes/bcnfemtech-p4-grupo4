7fda94f708077833ca540531a4f9414921610b5d
mavieuwu <mabigaming26@gmail.com>
2021-06-07 11:40:49 +0200

    first commit

---
+++ src/main/java/com/example/bcnfemtechp4grupo4/BcnfemtechP4Grupo4Application.java
@@ -0,0 +1,13 @@
+package com.example.bcnfemtechp4grupo4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BcnfemtechP4Grupo4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BcnfemtechP4Grupo4Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bcnfemtechp4grupo4/BcnfemtechP4Grupo4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bcnfemtechp4grupo4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BcnfemtechP4Grupo4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
