0e818d32901c7443189edbcaa979f858754ee0c4
Pepi Martín <mjmartinmonzonis@hotmail.com>
2021-06-09 14:56:16 +0200

    corrigiendo game controllers

--- src/main/java/com/example/bcnfemtechp4grupo4/controllers/GameController.java
+++ src/main/java/com/example/bcnfemtechp4grupo4/controllers/GameController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.awt.*;
@@ -17,31 +18,38 @@ import java.awt.*;
 
     @Autowired
     public GameController(GameService gameService) { this.gameService=gameService;}
+
     @GetMapping("/games")
        String listGames(Model model) {
         List<Game> games = gameService.allGames();
-        model.addAttribute("title" , title);
-        model.addAttribute("platform",platform);
-        model.addAttribute("year",year);
-        model.addAttribute("price",price);
-        model.addAttribute("etiqueta",etiqueta);
-        model.addAttribute("discount",discount);
-        model.addAttribute("pricediscount",pricediscount);
-        model.addAttribute("category",category);
-        model.addAttribute("publisher",publisher);
-        model.addAttribute("pegi",pegi);
-        model.addAttribute("pegicontentdescriptors",pegicontentdescriptors);
-        return "games/new";
+        model.addAttribute("title" , "Games List");
+        model.addAttribute("game",games);
+        return "games/all";
     }
     @GetMapping("/games/new")
         String getForm(Model model){
         Game game = new Game();
+        model.addAttribute("title","Create new game");
         model.addAttribute("game",game);
-        return "games/new";
+        return "games/edit";
     }
-    @PostMapping("/new")
+    @PostMapping("/games/new")
         public String addGame(@ModelAttribute Game game) {
         gameService.save(game);
         return "redirect:/games";
     }
+    @GetMapping("/games/edit/{id}")
+        public String editGame(Model model, @PathVariable Long id) {
+        Game game = gameService.findById(id);
+        model.addAttribute("game",game);
+        model.addAttribute("title","Edit game");
+        return "games/edit";
+    }
+    @GetMapping("/games/delete/{id}")
+        public String deleteGame(@PathVariable Long id) {
+        gameService.delete(id);
+        return "redirect:/games";
+    }
+
+
 }
