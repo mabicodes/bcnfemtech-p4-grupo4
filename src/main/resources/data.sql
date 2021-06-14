99e88771b5ecf8c74224438068c427f32ae376e6
Nicole Uy <mabigaming26@gmail.com>
2021-06-14 15:31:16 +0200

    añadiendo un juego mas en la lista con pepi

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,5 @@
 INSERT INTO games(title, year, category)
-VALUES ('Wii Sports', '2006', 'Sports'),
-       ('Super Mario Bros', '1985', 'Platform'),
-       ('Tetris', '1989', 'Puzle');
+VALUES  ('Wii Sports', '2006', 'Sports'),
+        ('Super Mario Bros', '1985', 'Platform'),
+        ('Tetris', '1989', 'Puzle'),
+        ('Mario Kart Wii', '2008', 'Racing');
