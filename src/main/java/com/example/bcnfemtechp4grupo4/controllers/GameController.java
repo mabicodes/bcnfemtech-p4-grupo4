18c4ff51db400850c969a1e7b39125af2df2cd0c
Nicole Uy <mabigaming26@gmail.com>
2021-06-18 14:30:32 +0200

    pagina /games /new

--- src/main/java/com/example/bcnfemtechp4grupo4/controllers/GameController.java
+++ src/main/java/com/example/bcnfemtechp4grupo4/controllers/GameController.java
@@ -22,6 +22,17 @@ import java.util.List;
     @Autowired
     public GameController(GameService gameService) { this.gameService=gameService;}
 
+
+
+
+    @GetMapping("/games")
+    String listGames(Model model) {
+        List<Game> games = gameService.allGames();
+        model.addAttribute("title" , "Games List");
+        model.addAttribute("games",games);
+        return "games/all";
+    }
+
     @GetMapping("/games/new")
        public String newGame(Model model) {
         Game game = new Game();
