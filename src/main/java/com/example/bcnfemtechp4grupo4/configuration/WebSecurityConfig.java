cd22b5d7e23c7d045f3e535aff292fd764df1447
Alisa <filonenkoalisa@gmail.com>
2021-06-29 15:12:54 +0200

    ponemos login y password

--- src/main/java/com/example/bcnfemtechp4grupo4/configuration/WebSecurityConfig.java
+++ src/main/java/com/example/bcnfemtechp4grupo4/configuration/WebSecurityConfig.java
@@ -1,17 +1,20 @@
-package com.example.bcnfemtechp4grupo4.configuration;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
-
+        package com.example.bcnfemtechp4grupo4.configuration;
+        import org.springframework.context.annotation.Configuration;
+        import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+        import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 @Configuration
-   public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
-
-     @Override
-     protected void configure(HttpSecurity http) throws Exception {
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/", "/categoria/**/**", "/games/", "/images/**", "/css/**", "/js/**", "/login").permitAll()
+                .antMatchers("/",
+                        "/games/",
+                        "/title/**/**",
+                        "/price/**",
+                        "/category/**",
+                        "/year/**",
+                        "/js/**",
+                        "/login").permitAll()
                 .anyRequest().authenticated()
                 .and().formLogin().permitAll()
                 .and().logout().permitAll();
