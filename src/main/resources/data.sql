e68207fc301fff573953ddb878944302e4e26bb3
Nicole Uy <mabigaming26@gmail.com>
2021-06-29 10:17:09 +0200

    cambios en css

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,3 +3,4 @@ VALUES  ('Wii Sports','19.99', '2006', 'Sports', 'default'),
         ('Super Mario Bros','14.99', '1985', 'Platform', 'default'),
         ('Tetris', '4.99','1989', 'Puzle', 'default'),
         ('Mario Kart Wii','9.99', '2008', 'Racing', 'default');
+
